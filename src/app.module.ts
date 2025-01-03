f427d741a506225228ac3100bbff51c9ceac6043
Cori1304-Seong <andyjm7739@gmail.com>
2025-01-04 02:33:55 +0900

    feat: Add wasabi code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WasabiController } from './wasabi/wasabi.controller';
+import { WasabiModule } from './wasabi/wasabi.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [WasabiModule],
+  controllers: [AppController, WasabiController],
   providers: [AppService],
 })
 export class AppModule {}
